c08905d45d53944ee6b7f452140d729330898fcb
Prachi Prakash Pawar <150327508+realprachipawar@users.noreply.github.com>
2026-04-26 22:51:11 +0530

    Delete Group_10 directory

--- Group_10/Source_Code/static/js/element_sdk.js
+++
@@ -1,8 +0,0 @@
-window.elementSdk = {
-    init: function(handler) {
-        console.log("Element SDK Initialized");
-        if (handler && handler.onConfigChange) {
-            handler.onConfigChange(handler.defaultConfig);
-        }
-    }
-};
